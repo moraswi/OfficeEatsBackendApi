f65cef0cf4b1d5ddd5d8fe9b172b7e06501aaab0
moraswi2 <moraswip97@gmail.com>
2024-10-15 14:58:22 +0200

    route

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace officeeatsbackendapi.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok("Welcome to OfficeEats API!");
+        }
+    }
+}
