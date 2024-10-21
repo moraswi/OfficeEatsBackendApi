b22d772cfe29934640236c796cdf3d2e163d63ba
moraswi2 <moraswip97@gmail.com>
2024-10-21 23:23:22 +0200

    ordere

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
 
 
 // DataContext
